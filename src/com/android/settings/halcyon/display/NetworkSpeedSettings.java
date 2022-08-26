1256cd1f7f6b21b8c58b5713e43d4861b8b62a4f
ezio84 <brabus84@gmail.com>
2022-08-26 22:41:53 +0700

    Settings: Features: Add Network speed indicator
    Co-authored-by: Anushek Prasal <anushekprasal@gmail.com>
    Signed-off-by: HeroBuxx <me@herobuxx.me>

---
+++ src/com/android/settings/halcyon/display/NetworkSpeedSettings.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright (C) 2023 Halcyon Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.halcyon.display;
+
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceGroup;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.SwitchPreference;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import com.android.settings.R;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settingslib.search.SearchIndexable;
+import com.android.settings.Utils;
+
+import java.util.Locale;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Collections;
+
+import com.android.settings.halcyon.preference.SecureSettingSwitchPreference;
+import com.android.settings.halcyon.preference.SecureSettingMainSwitchPreference;
+import com.android.settings.halcyon.preference.SystemSettingDropDownPreference;
+
+@SearchIndexable
+public class NetworkTrafficSettings extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private static final String TAG = "NetworkTrafficSettings";
+    private static final String STATUS_BAR_CLOCK_STYLE = "status_bar_clock";
+
+    private SecureSettingMainSwitchPreference mNetTraffic;
+    private SecureSettingSwitchPreference mNetTrafficAutohide;
+    private SystemSettingDropDownPreference mNetTrafficUnits;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.network_traffic_settings);
+        getActivity().setTitle(R.string.network_traffic_settings_title);
+
+        final ContentResolver resolver = getActivity().getContentResolver();
+
+        mNetTraffic = findPreference(Settings.Secure.NETWORK_TRAFFIC_MODE);
+        mNetTraffic.setOnPreferenceChangeListener(this);
+
+        mNetTrafficAutohide = findPreference(Settings.Secure.NETWORK_TRAFFIC_AUTOHIDE);
+        mNetTrafficAutohide.setOnPreferenceChangeListener(this);
+
+        mNetTrafficUnits = findPreference(Settings.Secure.NETWORK_TRAFFIC_UNITS);
+        mNetTrafficUnits.setOnPreferenceChangeListener(this);
+        int units = Settings.Secure.getInt(resolver,
+                Settings.Secure.NETWORK_TRAFFIC_UNITS, /* Mbps */ 1);
+        mNetTrafficUnits.setValue(String.valueOf(units));
+
+        final boolean enabled = Settings.Secure.getInt(resolver,
+                Settings.Secure.NETWORK_TRAFFIC_MODE, 0) == 1;
+        updateEnabledStates(enabled);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mNetTrafficUnits) {
+            int units = Integer.valueOf((String) newValue);
+            Settings.Secure.putInt(getActivity().getContentResolver(),
+                    Settings.Secure.NETWORK_TRAFFIC_UNITS, units);
+        }
+        return true;
+    }
+
+    private void updateEnabledStates(boolean enabled) {
+        mNetTrafficAutohide.setEnabled(enabled);
+        mNetTrafficUnits.setEnabled(enabled);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.HALCYON;
+    }
+
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider(R.xml.network_speed);
+}
